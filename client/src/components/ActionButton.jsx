8299a49a07d3b5a72df3029543b92b1826f5f117
Sylvain T <sylvaintagnabou@gmail.com>
2023-06-04 13:57:53 +0000

    .......

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Tilt from "react-parallax-tilt";
 
-const ActionButton = () => {
+import styles from '../styles'
+
+
+const ActionButton = ({imgUrl, handleClick, restStyles}) => {
   return (
-    <div>ActionButton</div>
+    <div className={`${styles.gameMoveBox} ${styles.flexCenter} ${styles.glassEffect} ${restStyles}`}>
+      <img src={imgUrl} alt="action_img" className={styles.gameMoveIcon} />
+    </div>
   )
 }
 
